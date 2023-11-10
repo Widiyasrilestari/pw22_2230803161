d8fc5bfda64a1a1ff766c87ab1332223ec6389a2
Widiyasrilestari12 <145080903+Widiyasrilestari@users.noreply.github.com>
2023-11-10 07:19:44 -0600

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,12 @@
+<?php
+
+$nrp = "173040888";
+$nama = "widya Srilestari";
+$umur = 21;
+$nilai = 75.75;
+
+echo "Nrp   : ".$nrp . "</br>";
+echo "Nama  : $nama</br>";
+print "Umur : ".$umur; print "</br>";
+printf ("Nilai  : %.3f</br>", $nilai);
+?>
---
+++ latihan3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en"> 
+    <meta charset="UTF-8">
+    <title> Modul 2 - Latihan 1</title>
+    <tr>
+        <th>Kolom 1</th>
+        <th>Kolom 2</th>
+        <th>Kolom 3</th>
+        <th>Kolom 4</th>
+        <th>Kolom 5</th>
+    </tr>
+    <!-- TULIS SCRIPT PHP DIBAWAH INUI-->
+</table>
+</body>
+</html>
